e12b46f5102ccd627db4c966f8dc32f02b10f680
Shakstetic <amshyaswad@gmail.com>
2025-07-02 23:45:23 +0800

    feat: dynamic styling at design token level, class level

--- components/workout/ExerciseCard.tsx
+++ components/workout/ExerciseCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography, Space, Divider } from "antd";
+import { Card, Typography, Space, Divider, Flex } from "antd";
 import { type Workout } from "../stores/workoutDataStore";
 
 // interface for type safety of component props, which itself uses a defined interface for Workout
@@ -10,14 +10,20 @@ interface ExerciseCardProps {
 export default function ExerciseCard(props: Readonly<ExerciseCardProps>) {
   const { w } = props;
   return (
-    <Card key={w.key} size="small">
-      <Typography.Paragraph strong>Date: {w.date}</Typography.Paragraph>
-      <Typography.Paragraph>Exercise: {w.exercise}</Typography.Paragraph>
+    <div className="card-typography">
+      <Card key={w.key} size="small">
+      <Flex justify="space-between">
+        <Typography.Paragraph strong>Exercise: {w.exercise}</Typography.Paragraph>
+        <Typography.Paragraph>Date: {w.date}</Typography.Paragraph>
+        
+      </Flex>
       <Space split={<Divider type="vertical" />}>
         <Typography.Paragraph>Sets: {w.sets}</Typography.Paragraph>
         <Typography.Paragraph>Reps: {w.reps}</Typography.Paragraph>
         <Typography.Paragraph>Weight: {w.weight} kg</Typography.Paragraph>
       </Space>
     </Card>
+    </div>
+    
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,20 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
+import { useLightDarkModeState } from "@/components/stores/lightDarkModeState";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
 	config.autoAddCss = false; // Prevent fontawesome icons from autoloading css first
+
+	// use Zustand globally persisted state to change the component design tokens
+	const isDarkMode = useLightDarkModeState((state) => state.isDarkMode);
+
+	// workaround for antD not having design token to edit cell bg
+	React.useEffect(() => {
+		document.body.classList.toggle('dark', isDarkMode);
+		document.body.classList.toggle('light', !isDarkMode);
+	}, [isDarkMode]);
 
 	// Wrap your providers here
 	return (
@@ -17,8 +28,16 @@ export default function App({ Component, pageProps }: AppProps) {
 				},
 				components: {
 					Table: {
-						headerBg: 'var(--primary)', 
-						headerSplitColor: 'var(--background)'
+						headerBg: isDarkMode ? 'var(--secondary)' : 'var(--primary)', 
+						headerColor: isDarkMode ? 'var(--textDarkMode)' : 'var(--text)',
+						headerSplitColor: isDarkMode ? 'var(--textDarkMode)' : 'var(--text)',
+						bodySortBg: isDarkMode ? 'var(--primary)' : '#fff',
+						rowHoverBg: isDarkMode ? '#2a2a2a' : '#f5f5f5',
+						colorText: isDarkMode ? 'var(--textDarkMode)' : 'var(--text)'
+					},
+					Card: {
+						colorBgContainer: isDarkMode ? 'var(--secondary)' : 'var(--foreground)',
+						colorText: isDarkMode ? 'var(--textDarkMode)' : 'var(--text)'
 					}
 				}
 			}}
